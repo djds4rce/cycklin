cb4ec11c912666b09a8acb6b7a67998cc5b2500d
djds4rce <djd4rce@gmail.com>
2013-02-02 16:23:14 +0530

    bleh

--- app/controllers/cycles_controller.rb
+++ app/controllers/cycles_controller.rb
@@ -1,6 +1,10 @@
 class CyclesController < ApplicationController
 
   def index
-
+    @cycles = Cycle.where(:flag=>true).page(params[:page]).per(20)
+    respond_to do |format|
+      format.js
+      format.html 
+    end
   end
 end
