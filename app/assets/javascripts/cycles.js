ef9ce656da26d271b39590b78072b2eb097cd870
djds4rce <djd4rce@gmail.com>
2013-02-02 16:19:13 +0530

    assets

---
+++ app/assets/javascripts/cycles.js
@@ -0,0 +1,30 @@
+(function() {
+  var page = 1,
+  loading = false;
+
+  function nearBottomOfPage() {
+    return $(window).scrollTop() > $(document).height() - $(window).height() - 200;
+  }
+
+  $(window).scroll(function(){
+    if (loading) {
+      return;
+    }
+
+    if(nearBottomOfPage()) {
+      loading=true;
+      page++;
+      $.ajax({
+        url: '/cycles?page=' + page,
+        type: 'get',
+        dataType: 'script',
+        success: function() {
+          $(window).sausage('draw');
+          loading=false;
+        }
+      });
+    }
+  });
+
+  $(window).sausage();
+}());
